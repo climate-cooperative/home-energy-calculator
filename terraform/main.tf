2733bd6e82ed5d3ca44ab2e8fa5bc886d02072de
rgraue <rgraue@outlook.com>
2024-04-02 14:09:49 -0700

    new bucket name

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "zwell-static-hosting-bucket" {
-  bucket = "app2.zwellhome.com"
+  bucket = "app.zwell.io"
 }
 
 resource "aws_s3_bucket_website_configuration" "static-hosting-config" {
@@ -36,27 +36,6 @@ resource "aws_s3_bucket_policy" "static-hosting-policy" {
         Resource = [
           "${aws_s3_bucket.zwell-static-hosting-bucket.arn}/*"
         ]
-        Condition = {
-          "IpAddress" = {
-            "aws:SourceIp" = [
-              "173.245.48.0/20",
-              "103.21.244.0/22",
-              "103.22.200.0/22",
-              "103.31.4.0/22",
-              "141.101.64.0/18",
-              "108.162.192.0/18",
-              "190.93.240.0/20",
-              "188.114.96.0/20",
-              "197.234.240.0/22",
-              "198.41.128.0/17",
-              "162.158.0.0/15",
-              "104.16.0.0/13",
-              "104.24.0.0/14",
-              "172.64.0.0/13",
-              "131.0.72.0/22"
-            ]
-          }
-        }
       }
     ]
   })
@@ -64,3 +43,24 @@ resource "aws_s3_bucket_policy" "static-hosting-policy" {
 
 # IP Adresses are cloudflare's proxy ips
 # https://www.cloudflare.com/ips-v4/#
+# Condition = {
+#           "IpAddress" = {
+#             "aws:SourceIp" = [
+#               "173.245.48.0/20",
+#               "103.21.244.0/22",
+#               "103.22.200.0/22",
+#               "103.31.4.0/22",
+#               "141.101.64.0/18",
+#               "108.162.192.0/18",
+#               "190.93.240.0/20",
+#               "188.114.96.0/20",
+#               "197.234.240.0/22",
+#               "198.41.128.0/17",
+#               "162.158.0.0/15",
+#               "104.16.0.0/13",
+#               "104.24.0.0/14",
+#               "172.64.0.0/13",
+#               "131.0.72.0/22"
+#             ]
+#           }
+#         }
