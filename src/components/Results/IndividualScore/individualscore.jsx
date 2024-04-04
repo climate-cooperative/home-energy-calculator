01ede8ae04235656411df4a9d823720b636c7b60
cooper-roper <cooproper@hotmail.com>
2024-04-04 14:18:05 -0700

    Added content to result blocks

--- src/components/Results/IndividualScore/individualscore.jsx
+++ src/components/Results/IndividualScore/individualscore.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 import {
   Box,
   Card,
@@ -9,6 +10,7 @@ import {
   DialogTitle,
   DialogContent
 } from '@mui/material';
+import { documentText, bulb } from 'ionicons/icons';
 import { IonIcon } from '@ionic/react';
 import { chevronForwardOutline } from 'ionicons/icons';
 
@@ -24,12 +26,15 @@ const IndividualScore = (props) => {
   };
 
   const gradeColors = {
-    A: 'green',
-    B: 'gold',
-    C: 'red',
-    D: 'red',
-    E: 'red',
-    F: 'red'
+    'A+': 'green',
+    'A-': 'green',
+    'B+': 'gold',
+    'B-': 'gold',
+    'C+': 'red',
+    'C-': 'red',
+    'D+': 'red',
+    'D-': 'red',
+    'F': 'black'
   };
 
   return (
@@ -63,13 +68,78 @@ const IndividualScore = (props) => {
               <IonIcon icon={chevronForwardOutline} style={{ fontSize: '30px' }} />
             </Box>
             <hr style={{ color: 'lightgray' }} />
-            <Typography variant="body1">Current: {props.current}</Typography>
+            {/* <Typography variant="body1">Current: {props.current}</Typography> */}
+            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+              {props.content.map((item, index) => (
+                <Typography key={index} variant="body1" style={{ margin: '10px' }}>
+                  <IonIcon icon={item.icon}/> {item.title}
+                </Typography>
+              ))}
+            </div>
           </CardContent>
         </CardActionArea>
       </Card>
       <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>{props.title}</DialogTitle>
-        <DialogContent>{/* Add the content of the dialog here */}</DialogContent>
+        <DialogTitle>
+          <Box display="flex" alignItems="center">
+            <IonIcon icon={props.icon} style={{ fontSize: '30px', marginRight: '10px' }} />
+            <Typography variant="h5" component="div" style={{ fontWeight: 'bold' }}>
+              {props.title}
+            </Typography>
+            {/* vertical line */}
+            <div
+              style={{
+                borderLeft: '2px solid lightgray',
+                height: '20px',
+                marginRight: '10px',
+                marginLeft: '10px'
+              }}
+            />
+            <Typography
+              variant="h5"
+              component="div"
+              style={{ fontWeight: 'bold', color: gradeColors[props.grade] }}
+            >
+              Grade: {props.grade}
+            </Typography>
+          </Box>
+        </DialogTitle>
+        <DialogContent>
+          <hr style={{ color: 'lightgray' }} />
+          <Typography variant="h2"><IonIcon icon={documentText} style={{ marginRight: '10px' }}/> System Report Card</Typography>
+          <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+            {props.content.map((item, index) => (
+              <Typography key={index} variant="body1" style={{ marginRight: '10px' }}>
+                <IonIcon icon={item.icon}/> {item.title}
+              </Typography>
+            ))}
+          </div>
+          <Typography variant="h2" style={{margin: 10}}>
+            <IonIcon icon={bulb}/> Recommendations
+          </Typography>
+          <TableContainer component={Paper}>
+            <Table aria-label="Recommend table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Appliance(s)</TableCell>
+                  <TableCell align="right">Your {props.current}</TableCell>
+                  <TableCell align="right">{props.recommended}</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {props.rows.map((row) => (
+                  <TableRow key={row.name}>
+                    <TableCell component="th" scope="row">
+                      {row.name}
+                    </TableCell>
+                    <TableCell align="right">{row.userValue}</TableCell>
+                    <TableCell align="right">{row.recommendedValue}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </DialogContent>
       </Dialog>
     </div>
   );
