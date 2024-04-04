6f82e9ab7ea9c3213588ca1792146f7610caf5b9
rgraue <rgraue@outlook.com>
2024-04-03 19:22:33 -0700

    rm ttl

--- terraform/record.tf
+++ terraform/record.tf
@@ -6,7 +6,6 @@ resource "aws_route53_record" "zwell_calculator_app_record" {
   zone_id = data.aws_route53_zone.zwell_io_hosted_zone.id
   name = "app.zwell.io"
   type = "A"
-  ttl = 300
   alias {
     name = aws_cloudfront_distribution.zwell_home_energy_distro.domain_name
     zone_id = aws_cloudfront_distribution.zwell_home_energy_distro.hosted_zone_id
