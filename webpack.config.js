f9bfb349e1e3a13846237a89bed282dcd0d06cd7
rgraue <rgraue@outlook.com>
2024-04-08 20:35:39 -0700

    test upload

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 cache: false,
  output: {
    path: path.join(__dirname, '/dist'),
-   filename: 'index.bundle.js'
+   filename: 'index.bundle.js',
  },
   // webpack 5 comes with devServer which loads in development mode
  devServer: {
@@ -30,7 +30,12 @@ cache: false,
      {
        test: /\.css$/,
        use: ['style-loader', 'css-loader']
-     }
+     },
+     {
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
+      loader: "url-loader",
+      options: { limit: false },
+    },
    ]
  },
  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
